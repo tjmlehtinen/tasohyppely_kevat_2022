48ce5e7392c751a6fa3b5682b3edda85cd9aa538
Your Name <you@example.com>
2022-04-11 18:16:19 +0300

    hyppii

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // muuttujat liikkumista varten
     float speed = 5.0f;
-    private float horizontalMovement = 0.0f;
+    float horizontalMovement = 0.0f;
+    // muuttuja hyppyvoimalle
+    public float jumpForce = 4.0f;
     // muuttuja Rigidbodylle, jota liikutetaan
     public Rigidbody2D myRigidbody;
     // Start is called before the first frame update
@@ -19,8 +22,13 @@ public class PlayerMovement : MonoBehaviour
     {
         // haetaan näppäimistöltä tieto halutusta sivuttaisliikkeestä
         horizontalMovement = Input.GetAxis("Horizontal");
-        // testataan kirjoittamalla konsoliin
-        Debug.Log(horizontalMovement);
+        if (Input.GetButtonDown("Jump"))
+        {
+            // tehdään vektori, joka annetaan hyppykäskyllä pelihahmolle
+            Vector2 jumpVector = new Vector2(0, jumpForce);
+            // lisätään pelaajan rigidbodyyn vektorin voima impulssina
+            myRigidbody.AddForce(jumpVector, ForceMode2D.Impulse);
+        }
     }
     // tasaisesti pyörivä luuppi liikettä varten
     void FixedUpdate()
